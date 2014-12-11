7a949c6a8ea61654369b819e732cbe708323283a
Ubuntu <rstropek@DockerClientRS.DockerClientRS.f5.internal.cloudapp.net>
2014-12-11 14:21:30 +0000

    Update

--- configureDockerHost.sh
+++ configureDockerHost.sh
@@ -1,4 +1,6 @@
 apt-get update
+
+# Install and configure Samba for file sharing
 apt-get install -y samba
 mv /etc/samba/smb.conf /etc/samba/smb.conf.backup
 printf "[global]\nworkgroup = DOCKERSAMPLE\nsecurity = user\nmap to guest = Bad Password\nguest account = nobody\n\n[appSrc]\ncomment = App Files\npath = /app/src\nbrowsable = yes\nguest ok = yes\nread only = no\ncreate mask = 0755\n" \
--- configureLinuxClient.sh
+++ configureLinuxClient.sh
@@ -1,8 +1,14 @@
 apt-get update
+
+# Install nodejs and npm
 apt-get install -y nodejs
 apt-get install -y npm
 npm install npm -g
 ln -s /usr/bin/nodejs /usr/bin/node
+
+# Install Azure XPlat tools
 npm install azure-cli -g
+
+# Install Docker
 curl -sSL https://get.docker.com/ubuntu/ | sh
 
