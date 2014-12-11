fbf677cb38da5981b022b4a62ea7f3386244b3ca
Ubuntu <rstropek@DockerClientRS.DockerClientRS.f5.internal.cloudapp.net>
2014-12-11 14:15:45 +0000

    Update

---
+++ openDockerHostShell.sh
@@ -0,0 +1,2 @@
+sudo ssh -i sshkey.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+dockersample@dockersamplehost.cloudapp.net
---
+++ openLinuxClientShell.sh
@@ -0,0 +1,2 @@
+sudo ssh -i sshkey.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+dockersample@dockersamplelinuxclient.cloudapp.net
