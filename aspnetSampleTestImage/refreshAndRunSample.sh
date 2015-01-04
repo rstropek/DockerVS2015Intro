79d89bddc42747ee0b03d3630f4d6a8dd7ccd3d4
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-01-04 17:44:59 +0100

    .

--- aspnetSampleTestImage/refreshAndRunSample.sh
+++ aspnetSampleTestImage/refreshAndRunSample.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Get latest version from GitHub
 git pull https://github.com/aspnet/Home.git
+
+# Restore packages
 cd samples/HelloMvc/
 kpm restore
+
+# Start kestrel web server
 k kestrel
 
