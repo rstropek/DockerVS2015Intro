d5698789cc883af5d329a811238be5664aa8e81b
Rainer Stropek <rainer@timecockpit.com>
2016-02-14 11:46:55 +0100

    Enhance slides

--- dockerDemos/00-AzureARM/configure.sh
+++ dockerDemos/00-AzureARM/configure.sh
@@ -7,4 +7,4 @@ curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install some important tools globaly
-sudo npm install -g typescript gulp grunt-cli tsd yo
+sudo npm install -g typescript gulp grunt-cli tsd yo generator-aspnet
