602792beb3ece7fc0916f150cb307473ee595d03
Rainer Stropek <rainer@timecockpit.com>
2016-02-15 08:55:53 +0100

    Add install script

---
+++ dockerDemos/00-AzureARM/installDocker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo echo deb https://apt.dockerproject.org/repo ubuntu-wily main > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get purge lxc-docker
+
+sudo apt-get install linux-image-extra-$(uname -r)
+
+sudo apt-get install docker-engine
