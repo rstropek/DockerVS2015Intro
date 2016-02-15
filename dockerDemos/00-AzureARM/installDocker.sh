913e046620a084c92727248e285749fe44e03d87
Rainer Stropek <rainer@timecockpit.com>
2016-02-15 09:31:38 +0100

    Fix script issues

--- dockerDemos/00-AzureARM/installDocker.sh
+++ dockerDemos/00-AzureARM/installDocker.sh
@@ -3,10 +3,10 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo deb https://apt.dockerproject.org/repo ubuntu-wily main > /etc/apt/sources.list.d/docker.list
+sudo echo deb https://apt.dockerproject.org/repo ubuntu-wily main | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get purge lxc-docker
 
 sudo apt-get install linux-image-extra-$(uname -r)
 
-sudo apt-get install docker-engine
+sudo apt-get install -y docker-engine
