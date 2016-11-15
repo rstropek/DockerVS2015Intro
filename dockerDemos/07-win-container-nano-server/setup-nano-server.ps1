bc5147d67c72142aad2fc522f117ff0c8abc3a1c
Rainer Stropek <rainer@software-architects.at>
2016-11-15 09:25:49 +0100

    Refresh Nano Server-realted content (slides, samples)

--- dockerDemos/07-win-container-nano-server/setup-nano-server.ps1
+++ dockerDemos/07-win-container-nano-server/setup-nano-server.ps1
@@ -11,6 +11,7 @@ $targetPath = $temp + "\NanoServerVM.vhd"
 $computerName = "dockerhost"
 $secure_string_pwd = convertto-securestring "pass" -asplaintext -force
 $vmSwitch = "External"
+[Environment]::SetEnvironmentVariable("TEMP", "c:\temp\tempFolder", "Process")
 
 # Stop and remove existing VM
 $vm = Get-VM -Name $computerName -ErrorAction SilentlyContinue
@@ -36,8 +37,11 @@ Import-Module .\NanoServerImageGenerator -Verbose
 # Create Nano Server ISO image
 New-NanoServerImage -Edition Standard -DeploymentType Guest -MediaPath $winServerInstallRoot `
     -BasePath .\Base -TargetPath $targetPath -ComputerName $computerName -AdministratorPassword $secure_string_pwd `
-    -Packages Microsoft-NanoServer-Guest-Package -MaxSize 100GB `
-    -Compute -Clustering -Containers -EnableRemoteManagementPort -Verbose
+    -ServicingPackagePath "C:\Users\r.stropek\Downloads\ServiceUpdates\Windows10.0-KB3197954-x64.cab", `
+        "C:\Users\r.stropek\Downloads\ServiceUpdates\Windows10.0-KB3199986-x64.cab", `
+        "C:\Users\r.stropek\Downloads\ServiceUpdates\Windows10.0-KB3200970-x64.cab" `
+    -Package Microsoft-NanoServer-Guest-Package -MaxSize 100GB `
+    -Compute -Containers -EnableRemoteManagementPort -Verbose
 
 # Create and start VM with new Nano Server image
 $vm = New-VM -Name $computerName -VHDPath $targetPath -Generation 1 -Verbose -MemoryStartupBytes 2GB -SwitchName $vmSwitch
@@ -50,15 +54,6 @@ do {
 }
 until ($vmIP)
 
-# Download Docker (for details see https://msdn.microsoft.com/en-us/virtualization/windowscontainers/deployment/deployment_nano
-# and https://msdn.microsoft.com/en-us/virtualization/windowscontainers/quick_start/quick_start_windows_server)
-if (!(Test-Path -Path "$temp\dockerd.exe")) {
-    Invoke-WebRequest https://aka.ms/tp5/b/dockerd -OutFile "$temp\dockerd.exe"
-}
-if (!(Test-Path -Path "$temp\docker.exe")) {
-    Invoke-WebRequest https://aka.ms/tp5/b/docker -OutFile "$temp\docker.exe"
-}
-
 # Open session to VM
 net start WinRM
 Set-Item WSMan:\localhost\Client\TrustedHosts $vmIP -Force
@@ -66,22 +61,16 @@ $cred = new-object -typename System.Management.Automation.PSCredential -argument
 $session = New-PSSession -ComputerName $vmIP -Credential $cred
 
 # Copy docker to vm and install it
-Invoke-Command -Session $session -ScriptBlock { New-Item -Type Directory -Path 'C:\docker\' }
-Copy-Item -ToSession $session "$temp\dockerd.exe" 'C:\docker\'
-Copy-Item -ToSession $session "$temp\docker.exe" 'C:\docker\'
 Invoke-Command -Session $session -ScriptBlock { netsh advfirewall firewall add rule name="Docker daemon" dir=in action=allow protocol=TCP localport=2375 }
 Invoke-Command -Session $session -ScriptBlock { netsh advfirewall firewall add rule name="HTTP" dir=in action=allow protocol=TCP localport=80 }
-Invoke-Command -Session $session -ScriptBlock { new-item -Force -type File c:\ProgramData\docker\config\daemon.json -value '{ "hosts": ["tcp://0.0.0.0:2375", "npipe://"] }' }
-Invoke-Command -Session $session -ScriptBlock { C:\docker\dockerd.exe --register-service; Start-Service Docker }
-
-# Download and install Nano Server container image
-Invoke-Command -Session $session -ScriptBlock { Install-PackageProvider ContainerImage -Force  }
-Invoke-Command -Session $session -ScriptBlock { Install-ContainerImage -Name NanoServer -Verbose -Force }
-Invoke-Command -Session $session -ScriptBlock { Restart-Service Docker }
-Invoke-Command -Session $session -ScriptBlock { c:\docker\docker.exe tag nanoserver:10.0.14300.1016 nanoserver:latest }
+Invoke-Command -Session $session -ScriptBlock { Install-Module -Name DockerMsftProvider -Repository PSGallery -Force }
+Invoke-Command -Session $session -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider }
+Invoke-Command -Session $session -ScriptBlock { Start "C:\Program Files\Docker\dockerd.exe" --register-service }
+Invoke-Command -Session $session -ScriptBlock { new-item -Type File c:\ProgramData\docker\config\daemon.json }
+Invoke-Command -Session $session -ScriptBlock { Add-Content 'c:\programdata\docker\config\daemon.json' '{ "hosts": ["tcp://0.0.0.0:2375", "npipe://"] }' }
+Restart-VM $vm
 
 # That's it. Nano Server with Docker is running and listening on port 2375
-
 
 # Helper commands
 # Enter-PSSession -Session $session
