742caa5444a502ca7f8b4eae423c0441ec35848f
Rainer Stropek <rainer@software-architects.at>
2018-03-20 07:47:22 +0100

    Add .NET Core Sample using Postgres

---
+++ dockerDemos/labs/NetCorePostgres/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkWebApi.Models;
+using Newtonsoft.Json;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace EntityFrameworkWebApi
+{
+    public class Startup
+    {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    // Configure JSON serialization so that it ignores loops
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+            // We use SQL Server Express LocalDB for this example
+            services.AddDbContext<AddressBookContext>(options => options.UseNpgsql(
+                configuration["ConnectionStrings:DefaultConnection"]));
+
+            // Configure AutoMapper, a very useful component for copying data
+            // between objects. For details see http://automapper.org/
+            var config = new MapperConfiguration(cfg => cfg.CreateMissingTypeMaps = true);
+            var mapper = config.CreateMapper();
+            services.AddSingleton<IMapper>(mapper);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
