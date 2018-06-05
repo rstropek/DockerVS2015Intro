540abc64d4cd193f059614ef25e70d80e02bb9bf
Rainer Stropek <rainer@software-architects.at>
2018-06-05 14:32:02 +0200

    Add .NET Core 2.1 sample with SQL access and docker-compose file

---
+++ dockerDemos/labs/DotNetCoreSqlSample/DotNetCoreDemo.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace DotNetCoreDemo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            using (var conn = new SqlConnection("Server=db;Database=master;UID=sa;Password=P@ssw0rd!123"))
+            {
+                await conn.OpenAsync();
+                var result = await conn.QuerySingleAsync<int>("SELECT 42 AS AnswerToEverything");
+                return new string[] { result.ToString() };
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ dockerDemos/labs/DotNetCoreSqlSample/DotNetCoreDemo.WebApi/Dockerfile
@@ -0,0 +1,9 @@
+﻿FROM microsoft/dotnet:sdk AS builder
+COPY . /app/
+WORKDIR /app
+RUN dotnet publish -o ./out -c Release
+
+FROM microsoft/dotnet:2.1-aspnetcore-runtime-alpine
+WORKDIR /app
+COPY --from=builder /app/out/ .
+CMD ["dotnet", "DotNetCoreDemo.WebApi.dll"]
